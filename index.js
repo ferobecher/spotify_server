7850e6d8323388793058ddb223e846347bd67f40
František Becher <58790946+ferobecher@users.noreply.github.com>
2024-09-09 17:09:17 +0200

    CORS update

--- index.js
+++ index.js
@@ -8,6 +8,13 @@ port = 3000;
 
 
 //express code
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', "*");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
+
 app.get('/api/artist/:artist_id/:album_id', async (req, res) => {
     try {
         const songs = await getSongs(req.params.album_id);
